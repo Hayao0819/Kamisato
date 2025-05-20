80891c1cbfd8ca8a81e73b2fc39f8fc899c73830
hayao <shun819.mail@gmail.com>
2025-05-21 02:41:26 +0900

    docs: Update ayaka docs

--- ayaka/README.md
+++ ayaka/README.md
@@ -10,6 +10,46 @@ to Ayato or Blinky.
 - Ayaka works as client of blinkyd
 - Signing packages with GPG key
 
+## 動作環境
+
+AyakaはArchLinux環境下でのみ動作します。Manjaroでの動作はテストしていません。CachyOS版のPacmanでは動作しません。
+
+## 使い方
+
+### 設定ファイル
+
+`ayaka`は2つの設定ファイルで動作が決定されます。
+
+#### `.ayakarc.json`
+
+CLI設定のためのファイル
+
+```json
+{
+    "repodir": "../example/src/myrepo",
+    "destdir": "../example/out"
+}
+```
+
+#### `repo.json`
+
+リポジトリ設定のためのファイル
+
+```json
+{
+    "name": "myrepo",
+    "maintainer": "hayao <shun819.mail at gmail.com>",
+    "server": "example.com/myrepo"
+}
+```
+
+サンプルは[../example/src/myrepo/repo.json](../example/src/myrepo/repo.json)にあります。
+
+### サブコマンド
+
+- `build` 全てのパッケージをchroot環境内でビルドします
+- `list` パッケージの一覧を表示します
+
 ## Todo
 
 - Mirroring repository to another server
--- conf/repo.go
+++ conf/repo.go
@@ -1,28 +1,13 @@
 package conf
 
 type RepoConfig struct {
-	Name       string `koanf:"name"`
-	Maintainer string `koanf:"maintainer"`
-	Server     string `koanf:"server"`
+	Name       string   `koanf:"name"`
+	Maintainer string   `koanf:"maintainer"`
+	Server     string   `koanf:"server"`
+	// AurPkgs    []string `koanf:"aurpkgs"`
 }
 
-// func LoadRepoConfig(repodir string, config *RepoConfig) error {
-// 	viper.SetConfigName("repo")
-// 	viper.AddConfigPath(repodir)
-
-// 	if err := viper.ReadInConfig(); err != nil {
-// 		return err
-// 	}
-
-// 	if err := viper.Unmarshal(config); err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
-
 func LoadRepoConfig(repodir string) (*RepoConfig, error) {
-	// return loadConfigWithDir[RepoConfig]([]string{repodir}, []string{"repo.json"})
-
 	return loadConfig[RepoConfig](
 		[]string{repodir},
 		[]string{"repo.json"},
