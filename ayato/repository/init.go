2d33731877e2633206de0ff8b81450da57427818
hayao <shun819.mail@gmail.com>
2025-05-22 11:54:23 +0900

    refactor: Extract S3 initialization logic into initPkgBinaryStore function

--- ayato/repository/init.go
+++ ayato/repository/init.go
@@ -10,27 +10,11 @@ import (
 	"github.com/Hayao0819/Kamisato/conf"
 )
 
-func New(cfg *conf.AyatoConfig) (*Repository, error) {
-	var db PkgNameStoreProvider
+func initPkgBinaryStore(cfg *conf.AyatoConfig) (PkgBinaryStoreProvider, error) {
+	var bin PkgBinaryStoreProvider
 	var err error
 
-	dsn, err := cfg.Database.DSN()
-	if err != nil {
-		slog.Debug("Failed to get DSN", "error", err)
-	}
-
-	if dsn != "" {
-		slog.Warn("Using SQL is still experimental, please use with caution")
-		db, err = sql.NewSql(cfg.Database.Driver, dsn)
-	} else {
-		db, err = kv.NewBadger(cfg.DbPath())
-	}
-	if err != nil {
-		return nil, err
-	}
-
 	// Check if S3 is enabled
-	var bin PkgBinaryStoreProvider
 	if cfg.StorageType == "s3" {
 		slog.Warn("Using S3 is still experimental, please use with caution")
 		bin, err = s3.NewS3(&cfg.AWSS3)
@@ -43,6 +27,46 @@ func New(cfg *conf.AyatoConfig) (*Repository, error) {
 	// Fallback to localfs if S3 is not enabled
 	if bin == nil {
 		bin = localfs.NewLocalPkgBinaryStore(cfg)
+	}
+
+	return bin, nil
+}
+
+func initMetaStore(cfg *conf.AyatoConfig) (PkgNameStoreProvider, error) {
+	var db PkgNameStoreProvider
+	var err error
+
+	dsn, err := cfg.Database.DSN()
+	if err != nil {
+		slog.Debug("Failed to get DSN", "error", err)
+	}
+
+	if cfg.DatabaseType == "external" {
+		slog.Warn("Using SQL is still experimental, please use with caution")
+		db, err = sql.NewSql(cfg.Database.Driver, dsn)
+	} else {
+		db, err = kv.NewBadger(cfg.DbPath())
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func New(cfg *conf.AyatoConfig) (*Repository, error) {
+	// Initialize the database
+	db, err := initMetaStore(cfg)
+	if err != nil {
+		slog.Error("Failed to create meta store", "error", err)
+		return nil, err
+	}
+
+	// Check if S3 is enabled
+	bin, err := initPkgBinaryStore(cfg)
+	if err != nil {
+		slog.Error("Failed to create binary store", "error", err)
+		return nil, err
 	}
 
 	return &Repository{
