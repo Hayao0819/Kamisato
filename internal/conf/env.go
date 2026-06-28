e0b9e099f6bf930d18a589a5c8f940d5d1d0345d
hayao <shun819.mail@gmail.com>
2026-06-28 17:16:08 +0900

    fix: Stop logging a missing .env as an error

--- internal/conf/env.go
+++ internal/conf/env.go
@@ -1,7 +1,17 @@
 package conf
 
-import "github.com/joho/godotenv"
+import (
+	"errors"
+	"io/fs"
 
+	"github.com/joho/godotenv"
+)
+
+// LoadEnv loads a .env file when one is present. A missing .env is the normal
+// case, not an error, so callers don't log noise on every invocation.
 func LoadEnv() error {
-	return godotenv.Load()
+	if err := godotenv.Load(); err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return err
+	}
+	return nil
 }
