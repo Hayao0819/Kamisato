ff324e50af126aa840e574631ea6f951a2cc3646
hayao <shun819.mail@gmail.com>
2026-07-02 16:12:13 +0900

    refactor: Move the hook uninstall command out of pkg into the ayaka and kayo CLIs

--- ayaka/cmd/hook/uninstall.go
+++ ayaka/cmd/hook/uninstall.go
@@ -1,10 +1,32 @@
 package hookcmd
 
 import (
+	"fmt"
+
+	"github.com/Hayao0819/Kamisato/pkg/pacman/alpm"
 	"github.com/Hayao0819/Kamisato/pkg/pacman/hook"
 	"github.com/spf13/cobra"
 )
 
 func hookUninstallCmd() *cobra.Command {
-	return hook.UninstallCmd(uploadHookFileName)
+	var dir, pacmanConf string
+	cmd := &cobra.Command{
+		Use:   "uninstall",
+		Short: "Remove the installed pacman hook",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			if dir == "" {
+				dir = alpm.HookDir(pacmanConf)
+			}
+			path, err := hook.Uninstall(dir, uploadHookFileName)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "removed %s\n", path)
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&dir, "dir", "", "hook directory (default: pacman.conf HookDir)")
+	cmd.Flags().StringVar(&pacmanConf, "pacman-config", "", "pacman.conf path for resolving HookDir")
+	return cmd
 }
