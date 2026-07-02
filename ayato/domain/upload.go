7e198a2af24494a48bb2dc8108bc39a32704c0b0
hayao <shun819.mail@gmail.com>
2026-07-02 16:21:52 +0900

    refactor: Type UploadFiles fields as the stream.SeekFile interface

--- ayato/domain/upload.go
+++ ayato/domain/upload.go
@@ -3,6 +3,6 @@ package domain
 import "github.com/Hayao0819/Kamisato/ayato/stream"
 
 type UploadFiles struct {
-	PkgFile *stream.FileStream
-	SigFile *stream.FileStream
+	PkgFile stream.SeekFile
+	SigFile stream.SeekFile
 }
